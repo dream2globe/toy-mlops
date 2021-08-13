94065295573456d0b2cb911d0884e98e1052364e
Seonghyeon Kang <shyeon@gmail.com>
2021-08-14 01:36:35 +0900

    added a producer

---
+++ app_kafka/data.py
@@ -0,0 +1,17 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from random import randint
+from sklearn.datasets import load_iris
+
+data = load_iris()
+
+def get_data():
+    length = data["data"].shape[0]
+    random_sample = randint(0, length-1)
+    message = {k: v for k, v in zip(data["feature_names"], data["data"][random_sample])}
+    return message
+
+if __name__ == "__main__":
+    print(get_data())
---
+++ app_kafka/producer.py
@@ -0,0 +1,37 @@
+from kafka import KafkaProducer
+import json
+from data import get_data
+import time
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+def get_partition(key, all, available):
+    return 0 # 파티션을 특정할 수 있음
+
+producer = KafkaProducer(
+    bootstrap_servers=["192.168.0.2:9092"],
+    value_serializer=json_serializer,
+    # compression_type='snappy',
+    partitioner=get_partition
+)
+
+if __name__ == "__main__":
+    try:
+        while True:
+            msg = get_data()
+            logger.info(f"send message: {msg}")
+            producer.send("mlops", msg)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info(f"stoped by a user")
+    finally:
+        producer.close()
